d326349164f4fc7bdcdb4285a0d1efdb1ef2c128
Caleb Clauson <calebclauson@gmail.com>
2026-04-28 10:08:16 -0500

    Added AI API test for Google Gemini.

--- example-ai-api-usage.py
+++ example-ai-api-usage.py
@@ -1,24 +1,9 @@
-import requests
+from google import genai
 
-API_key= "PASTE KEY HERE"
-model="translategemma:latest"
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
 
-def chat_with_model(token):
-    url = 'http://sushi.it.ilstu.edu:8080/api/chat/completions'
-    headers = {
-        'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json'
-    }
-    data = {
-      "model": model,
-      "messages": [
-        {
-          "role": "user",
-          "content": "Why is the sky blue?"
-        }
-      ]
-    }
-    response = requests.post(url, headers=headers, json=data)
-    return response.json()
-
-print(chat_with_model(API_key)['choices'][0]['message']['content'])
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
+)
+print(response.text)
